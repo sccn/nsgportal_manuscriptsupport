084d313be7501f495c97e14f1641459870c26d92
ramonmc <ram033@eng.ucsd.edu>
2019-11-01 15:52:51 -0700

    Modifications to include system info ans data saving

--- relica_localtest/relica_localtest.m
+++ relica_localtest/relica_localtest.m
@@ -1,6 +1,9 @@
 % Script running RELICA on a local computing resource for measuring
 % performance.
  
+% Get system info
+system = cpuinfo;
+
 % Make relica output folder 
 mkdir('relicaoutput'); 
 
@@ -14,6 +17,12 @@ tic % Start timer
 EEG = pop_relica(EEG,100,'runica','point', 'relicaoutput'); % Running RELICA
 elapsedtime = toc; % Elapsed time since timer started
 
-% Saving elapsed time
-save('elapsedtime.mat', 'elapsedtime');
+% Saving dataset
+EEG = pop_saveset( EEG, 'filename','wh_sub011_proc_wrelica.set','filepath',pwd);
+
+% Saving elapsed time and system info
+info.system = system;
+info.time = elapsedtime;
+save('info.mat', 'info');
+
 
